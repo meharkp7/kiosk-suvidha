6079fd3da1a740ac20659129352a7e6eb82fa025
meharkp7 <meharkapoor7@gmail.com>
2026-02-27 00:17:54 +0530

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.use(cookieParser())
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
+    origin: ['https://kiosk-suvidha.vercel.app', 'http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin'],
